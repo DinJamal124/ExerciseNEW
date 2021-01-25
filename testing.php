b03d26a4b16e8f4b66ffd010880dd3562e57b9f2
DinJamal124 <19FTT1151@student.pb.edu.bn>
2021-01-25 15:43:52 +0800

    commit

--- testing.php
+++ testing.php
@@ -1,3 +1,48 @@
 <?php
-    echo "Hello My Depressing Thoughts, Please Help Me!";
+    echo "Hello My Depressing Thoughts, Please Help Me! <br>";
 ?>
+
+<?php
+    function myTest(){
+        static $x = 0;
+        echo $x;
+        $x++;
+    }
+
+    myTest();
+    echo "<br>";
+    myTest();
+    echo "<br>";
+    myTest();
+?>
+
+<style>
+table, th, td{
+    border: 2px solid black;
+    border-collapse: collapse;
+    padding: 10px;
+    background-color: red;
+};
+</style>
+
+<table>
+
+    <tr>
+        <td>Hello</td>
+        <td>world</td>
+        <td>test124</td>
+    </tr>
+
+    <tr>
+        <td>hut</td>
+        <td>jastes</td>
+        <td>test124</td>
+    </tr>
+
+    <tr>
+        <td>huys</td>
+        <td>haiiss</td>
+        <td>test124</td>
+    </tr>
+
+</table>
